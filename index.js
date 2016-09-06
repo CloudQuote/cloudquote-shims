5d9a1e89d40d01f38b4a45b7dce4e570b1cf5610
Mark Dierolf <mdierolf@users.noreply.github.com>
2016-09-06 08:33:35 -0700

    initial import

---
+++ index.js
@@ -0,0 +1,18 @@
+export const Object = {
+ values: window.Object.values || function (obj) {
+  let vals = [];
+  for (let key in obj) {
+   if (obj.hasOwnProperty(key)) {
+    vals.push(obj[key]);
+   }
+  }
+  return vals;
+ },
+ entries: window.Object.entries || function (obj) {
+  var entrys = [];
+  for (var key in obj) {
+   if (obj.hasOwnProperty(key)) entrys.push([key, obj[key]]);
+  }
+  return entrys;
+ }
+}
