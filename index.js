4406d97074e5fd33eafe27ae044814ee04b98640
Mark Dierolf <mdierolf@users.noreply.github.com>
2016-12-28 16:35:02 -0800

    compatibility fix for webpack

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-export const Object = {
- values: window.Object.values || function (obj) {
-  var vals = [];
-  for (var key in obj) {
-   if (obj.hasOwnProperty(key)) {
-    vals.push(obj[key]);
+export {
+ Object: {
+  values: window.Object.values || function (obj) {
+   var vals = [];
+   for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+     vals.push(obj[key]);
+    }
    }
+   return vals;
+  },
+  entries: window.Object.entries || function (obj) {
+   var entrys = [];
+   for (var key in obj) {
+    if (obj.hasOwnProperty(key)) entrys.push([key, obj[key]]);
+   }
+   return entrys;
   }
-  return vals;
- },
- entries: window.Object.entries || function (obj) {
-  var entrys = [];
-  for (var key in obj) {
-   if (obj.hasOwnProperty(key)) entrys.push([key, obj[key]]);
-  }
-  return entrys;
  }
-};
+}
 
